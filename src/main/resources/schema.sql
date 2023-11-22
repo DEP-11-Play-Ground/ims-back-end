cab5ef59ca9695d09fa90df0b6e8b3fd3467a46d
Danidu Bogoda <danidubogoda95@gmail.com>
2023-11-22 16:45:49 +0530

    Updated schema file

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS teacher(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    contact VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS course(
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(50) NOT NULL,
+      duration_in_months INT NOT NULL
+);
+
+DROP TABLE teacher;
+
+CREATE TABLE IF NOT EXISTS teacher(
+                                      id INT AUTO_INCREMENT PRIMARY KEY,
+                                      name VARCHAR(100) NOT NULL,
+                                      contact VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS course(
+                                     id INT AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(50) NOT NULL,
+                                     duration_in_months INT NOT NULL
+);
